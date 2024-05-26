ab50e926326ffa2486930e2e18d6be5c48c70f83
vaibhav indure <109229271+vaibhavindure188@users.noreply.github.com>
2024-05-26 23:35:07 +0530

    created wishlist and used concept of event Deligation, bubbling and capturing in js

--- index.js
+++ index.js
@@ -1,65 +1,51 @@
-function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+let input = document.querySelector(".input");
+let addBtn = document.querySelector(".button");
+let todosContainer = document.querySelector(".todos-container");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let todo = input.value;
+  let key = Date.now();
+  todo = todo.trim();
+  if (todo.length > 0) {
+    todos.push({ id: key, todo, isComplete: false });
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos(todos);
+  input.value = "";
+});
+
+function mainLogin(e) {
+  let key = e.target.dataset.key;
+  let todoDelKey = e.target.dataset.todokey;  // imp point dataset key should not be in camelcase
+  console.log(key)
+//   if (key !== undefined)
+    todos = todos.map((todo) =>
+      todo.id == key ? { ...todo, isComplete: !todo.isComplete } : todo
+    );
+//   if (todoDelKey !== undefined)
+    todos = todos.filter((todo) => todo.id != todoDelKey);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  console.log(todoDelKey);
+  console.log(todos);
+  renderTodos(todos);
 }
 
-let todoInput = document.querySelector(".input");
-let addTodoButton = document.querySelector(".button");
-let showTodos = document.querySelector(".todos-container");
-let todo = "";
+todosContainer.addEventListener("click", mainLogin);
 
-let localData = JSON.parse(localStorage.getItem("todos"));
-let todoList = localData || [];
-
-addTodoButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    todo = todoInput.value;
-    if (todo.length > 0) {
-        todoList.push({
-            todo,
-            id: uuid(),
-            isCompleted: false
-        });
-        renderTodoList(todoList);
-        localStorage.setItem("todos", JSON.stringify(todoList));
-        todoInput.value = "";
-    }
-});
-
-showTodos.addEventListener("click", (e) => {
-    e.preventDefault();
-    let key = e.target.dataset.key;
-    let delTodoKey = e.target.dataset.todokey;
-    todoList = todoList.map((todo) =>
-        todo.id === key ? {
-            ...todo,
-            isCompleted: !todo.isCompleted
-        } : todo
-    );
-    todoList = todoList.filter((todo) => todo.id !== delTodoKey);
-    localStorage.setItem("todos", JSON.stringify(todoList));
-    console.log(todoList);
-    renderTodoList(todoList);
-});
-
-function renderTodoList(todoList) {
-    showTodos.innerHTML = todoList.map(
-        ({
-            todo,
-            id,
-            isCompleted
-        }) =>
-        `<div class="todo relative"> <input id="item-${id}" data-key=${id} class="t-checkbox t-pointer" type="checkbox" ${
-        isCompleted ? "checked" : ""
-      }> <label data-key=${id} class="todo-text t-pointer ${
-        isCompleted ? "checked-todo" : ""
-      }" for="item-${id}"> ${todo} </label> <button class="absolute right-0 button cursor">
+function renderTodos(todos) {
+  todosContainer.innerHTML = todos.map(
+    ({ id, todo, isComplete }) =>
+      `<div class='todo relative'><input data-key=${id} id=${id} type='checkbox' ${
+        isComplete ? "checked" : ""
+      }  class="t-checkbox t-pointer"> <label data-key=${id} for=${id}  class="todo-text t-pointer ${
+        isComplete ? "checked-todo" : ""
+      }">${todo}</label> <button class="absolute right-0 button cursor">
       <span data-todokey=${id}  class="del-btn material-icons-outlined">delete</span>
-            </button> </div>`
-    );
+            </button><div>`
+  ).join(' ');
 }
 
-renderTodoList(todoList);
+renderTodos(todos);
